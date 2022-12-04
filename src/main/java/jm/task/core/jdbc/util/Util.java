a5c370e2f912d181d529210827e17bcff78e856d
ValentinFedorov <106467201+ValentinFedorov@users.noreply.github.com>
2022-12-04 16:47:57 +0700

    Add files via upload
    Исправление замечаний по задаче 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,24 +7,14 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-    public Util () {
-
-    }
-
     public Connection getConnection () {
         Connection connection = null;
         try {
             Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             System.out.println("Драйвер не зарегистрировался");
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-        } catch (SQLException e) {
-            System.out.println("Подключение не установлено");
         }
         return connection;
     }
